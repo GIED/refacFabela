54e6568ee9f2b02639a3a716383584ea8479fb4b
PedroFabela <fabelapedro@gmail.com>
2021-10-08 23:35:34 -0500

    todo

---
+++ src/app/caja/pages/cierre-de-caja/cierre-de-caja.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cierre-de-caja',
+  templateUrl: './cierre-de-caja.component.html',
+  styleUrls: ['./cierre-de-caja.component.scss']
+})
+export class CierreDeCajaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/reportes/pages/reporte-abonos/reporte-abonos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-abonos',
+  templateUrl: './reporte-abonos.component.html',
+  styleUrls: ['./reporte-abonos.component.scss']
+})
+export class ReporteAbonosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
