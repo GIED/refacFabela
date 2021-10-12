f88aa1e2c527ed1b7e155c45bf501bacce74f826
PedroFabela <fabelapedro@gmail.com>
2021-10-11 23:21:52 -0500

    inicio de historial del producto

--- src/app/productos/pages/historial-producto/historial-producto.component.ts
+++ src/app/productos/pages/historial-producto/historial-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Product } from 'src/app/demo/domain/product';
+import { ProductService } from 'src/app/demo/service/productservice';
 
 @Component({
   selector: 'app-historial-producto',
@@ -6,10 +9,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historial-producto.component.scss']
 })
 export class HistorialProductoComponent implements OnInit {
+lineChartData:any;
+lineChartOptions:any;
+  products: Product[];
+  lineData:any;
+  lineOptions:any;
 
-  constructor() { }
+  constructor(private productService: ProductService, private messageService: MessageService,
+    private confirmationService: ConfirmationService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.productService.getProducts().then(data => this.products = data);
+
+    this.lineData = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+      datasets: [
+          
+          {
+              label: 'Ventas del mes',
+              data: [28, 48, 40, 19, 86, 27, 90],
+              fill: false,
+              backgroundColor: 'rgb(75, 192, 192)',
+              borderColor: 'rgb(75, 192, 192)',
+              tension: .4
+          }
+      ]
+  };
+
+  this.lineOptions = {
+      plugins: {
+          legend: {
+              labels: {
+                  fontColor: '#A0A7B5'
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: '#A0A7B5'
+              },
+              grid: {
+                  color:  'rgba(160, 167, 181, .3)',
+              }
+          },
+          y: {
+              ticks: {
+                  color: '#A0A7B5'
+              },
+              grid: {
+                  color:  'rgba(160, 167, 181, .3)',
+              }
+          },
+      }
+  };
+
+
   }
 
 }
