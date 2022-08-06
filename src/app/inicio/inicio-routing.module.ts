aff1e2f7e9703f21c92b5de4a64f88335aa2d7f4
PedroFabela <fabelapedro@gmail.com>
2022-08-05 20:05:15 -0500

    se modifico la pantalla de inicio de la aplcación

--- src/app/inicio/inicio-routing.module.ts
+++ src/app/inicio/inicio-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableroComponent } from './tablero/tablero.component';
+import { InicioGeneralComponent } from './inicio-general/inicio-general.component';
 
 
 const routes: Routes = [
   {
-    path:'',
-    children:[
-    {
-    path:'tablero',   component:TableroComponent,
-    },
-    
-     
-    ]
-    
+    path:'tablero',   component:TableroComponent,    
+  },
+  {
+    path:'inicio-general',   component:InicioGeneralComponent,    
   }
 
 ];
--- src/app/inicio/inicio.module.ts
+++ src/app/inicio/inicio.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { InicioRoutingModule } from './inicio-routing.module';
 import { TableroComponent } from './tablero/tablero.component';
 import { PrimeModule } from '../shared/prime/prime.module';
+import { InicioGeneralComponent } from './inicio-general/inicio-general.component';
 
 
 @NgModule({
   declarations: [
-    TableroComponent
+    TableroComponent,
+    InicioGeneralComponent
   ],
   imports: [
     PrimeModule,
