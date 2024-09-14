9ea53218ed334f1f452748cb64ffd63df23b8184
Pedro Fabela <47532745+pedrofabela@users.noreply.github.com>
2024-09-14 11:23:19 -0600

    se agrega un valor de 20 al tipo de cambio si el objeto viene vacio

--- src/app/administracion/pages/facturas-proveedor/facturas-proveedor.component.ts
+++ src/app/administracion/pages/facturas-proveedor/facturas-proveedor.component.ts
@@ -43,6 +43,14 @@ export class FacturasProveedorComponent implements OnInit {
 
   ngOnInit(): void {
     this.cargainicial();
+    this.datoCambio=new DataSerie();
+    this.proveedorService.getTipoCambioBM().subscribe(data2=>{
+      this.datoCambio=data2;
+      if(data2 == null || data2 == undefined){
+        this.datoCambio= new DataSerie();
+        this,this.datoCambio.dato=20;
+      }
+    })
 
 
   }
@@ -52,10 +60,7 @@ export class FacturasProveedorComponent implements OnInit {
 
     this.titulo='Registro de facturas del proveedor'
 
-    this.datoCambio=new DataSerie();
-    this.proveedorService.getTipoCambioBM().subscribe(data2=>{
-      this.datoCambio=data2;
-    })
+    
 
 
 
